96f386c3f527f1fe9673c2cba2e13bc728c401f3
guiyee <guillermodinanno@gmail.com>
2023-05-23 19:05:59 -0300

    clase de componentes 1 terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Footer } from "./components/layout/footer/Footer";
 import { Navbar } from "./components/layout/navbar/Navbar";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
@@ -6,14 +7,19 @@ import { ItemListContainer } from "./components/pages/itemListContainer/ItemList
 
  function App() {
   
+  const [nombre, setNombre] = useState("pepe")
+  const [ isDark, setIsDark ] = useState(false)
+
   /* PROPS --> Voy pasando propiedades y todo cae dentro de un objeto(props)*/
-  let nombre = "juan"
   let edad = 22
 
   return(
     <>
      <Navbar />
      <ItemListContainer nombre={nombre} edad={edad} />
+     <button onClick={()=>setNombre("maria")}>Cambiar nombre</button>
+     {/* Cambiar estado isDark al contrario de lo que tenga --> !isDark */}
+     <button onClick={()=>setIsDark(!isDark)}>{isDark ? "cambiar a claro": "cambiar a oscuro"}</button>
      <Footer />
     </>
   )
--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-
+/* Importando desde React Icons. "BS" (primeras letras del icono) + pegar nombre de icono en llaves */
+import { BsFillCartFill } from "react-icons/bs"
+import { Counter } from "../../common/counter/Counter"
 
 export const Navbar = () => {
 
   return (
     <div>
+
         <h4>Logo</h4>
         <ul>
             <li>Todos</li>
@@ -11,7 +14,14 @@ export const Navbar = () => {
             <li>Buzos</li>
             <li>Camperas</li>
         </ul>
-        <h3>Carrito</h3>
+
+        {/* Carrito con contador */}
+        <div>
+          {/* Le pasamos props en base a lo que especifique la libreria */}
+          <BsFillCartFill color="steelblue" size={"30px"}/>
+          <Counter />
+        </div>
+
     </div>
   )
 }
