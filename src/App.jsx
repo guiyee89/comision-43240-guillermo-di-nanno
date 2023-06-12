d65096e6876ddfe1a0f086658e3b74b8ad361369
guiyee <guillermodinanno@gmail.com>
2023-06-12 13:40:39 -0300

    Rutas, Navegacion, react-router-dom y Custom Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,25 @@
-import styled from 'styled-components/macro'
-import { useState } from "react";
-import { Navbar } from "./components/layout/navBar/Navbar";
 import { ItemListContainer } from "./components/pages/itemListContainer/ItemListContainer";
-import { TextField } from "@mui/material";
-import { CounterContainer } from "./components/common/counter/CounterContainer";
-import { FetchingDatosContainer } from './components/pages/fetchingDatos/FetchingDatosContainer';
-import { ProductDetailContainer } from './components/pages/productDetail/ProductDetailContainer';
-import { Fetching } from './components/pages/fetching/Fetching';
-import { Users } from './components/pages/users/Users';
-import { Posts } from './components/pages/posts/Posts';
-import { Comments } from './components/pages/comments/Comments';
+import { ProductDetailContainer } from "./components/pages/productDetail/ProductDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Layout } from "./components/layout/Layout";
+import { CartContainer } from "./components/pages/cart/CartContainer";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-/* LOS COMPONENTES SE ACTUALIZAN, CUANDO CAMBIAN LOS ESTADOS INTERNOS O CUANDO CAMBIA UNA PROP */
 function App() {
-
-  // const [montado, setMontado] = useState(false);
-  
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/category/:categoryName" element={<ItemListContainer />} />
+            <Route path="/productDetail/:id" element={<ProductDetailContainer />} />
+            <Route path="/cart" element={<CartContainer />} />
+            <Route path="*" element={<h1>404 not found</h1>} />
+          </Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
-{/* <Navbar /> */} {/* pass contador and setContador as props */}
-
-      {/* Montar o desmontar un componente */}
-      {/* {montado && <ItemListContainer /> }
-      <Button onClick={() => setMontado(!montado)}>Montar</Button> */}
-
-      {/* <CounterContainer /> */}
-
-      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-
-      {/* <FetchingDatosContainer /> */}
-
-      {/* <ProductDetailContainer /> */}
-
-      {/* <Fetching /> */}
-
-      {/* <Users /> */}
-      {/* <Posts /> */}
-      {/* <Comments /> */}
-
-// const Button = styled.button`
-//    width: fit-content;
-//  `
-
 export default App;
-
-
-
